b773819408cf99dfe0e75803e7481ebc6612e44b
Jess Lampe <jess.lampe@mavenwave.com>
2019-01-02 13:27:19 -0600

    Card changes

--- src/components/cards/cardlist.js
+++ src/components/cards/cardlist.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import CardSummary from './cardsummary'
+import  {Link} from 'react-router-dom'
 
 const CardList = ({cards}) => {
 	return (
 		<div className="card-list section">
 			{ cards && cards.map(card => {
 				return (
+					<Link to ={'/cards/' + card.id}>
+
 					<CardSummary card={card} key={card.id} />
+					</Link>
+
+
 					)
 			})}
 		</div>
