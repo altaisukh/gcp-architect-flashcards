768d03c9fc452d9840cacad8bebef6127a836eda
Jess Lampe <jess.lampe@mavenwave.com>
2019-01-02 15:04:22 -0600

    Created hiding card functionality

--- src/components/cards/carddetails.js
+++ src/components/cards/carddetails.js
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import { compose } from 'redux'
 
-const CardDetails = (props) => {
-	const {card} = props;
+class CardDetails extends Component {
+	constructor () {
+		super()
+		this.state = {
+			isHidden: true
+		}
+	}
+	toggleHidden () {
+		this.setState({
+			isHidden: !this.state.isHidden
+		})
+	}
+	render() {
+		const {card} = this.props;
+		console.log(this.state)
+		if (card) {
+			let cardback;
+			let cardbutton;
+			if (!this.state.isHidden) {
+				cardback = <div className="card-content lighten-4 grey-text"><span className="card-title">{card.back}</span></div>;
+				cardbutton = <button className="waves-effect waves-light pink btn" onClick={this.toggleHidden.bind(this)} >Hide Answer</button>
+			} else {
+				cardback = <div></div>
+				cardbutton = <button className="waves-effect waves-light pink btn" onClick={this.toggleHidden.bind(this)} >Show Answer</button>
 
-	if (card) {
-		return (
-		<div className="container section card-details">
-			<div className="card z-depth-0">
-				<div className="card-content">
-					<span className="card-title">{card.front}</span>
+			}
+			return (
+				<div className="container section card-details">
+					<div className="card z-depth-0">
+						<div className="card-content">
+							<span className="card-title">{card.front}</span>
+						</div>
+						 <div>
+						 	{cardback}
+       					 </div>
+					</div>
+					{cardbutton}
 				</div>
-				<div className="card-content lighten-4 grey-text">
-					<span className="card-title">{card.back}</span>
-				</div>
-			</div>
-		</div>
-	)
-	} else {
-		return (
-			<div className="container center">
+
+				)
+		} else {
+			return (
+				<div className="container center">
 				<p>Loading data...</p>
-			</div>
-		)
+				</div>
+				)
+		}
 	}
 }
 
