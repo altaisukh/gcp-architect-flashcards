0a53fa9d5014f11f35938dc68bb04e1b70870b86
Jess Lampe <jess.lampe@mavenwave.com>
2019-01-03 22:37:50 -0600

    Added auth support

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -2,34 +2,42 @@ import React, { Component } from 'react'
 import { signIn } from '../../store/actions/authActions'
 import { connect } from 'react-redux'
 import {compose} from 'redux'
+import firebase from '../../config/fbConfig'
 
+const provider = new firebase.auth.GoogleAuthProvider();
 
 export class SignIn extends Component {
 	render () {
-		console.log(this.props)
+		firebase.auth().signInWithPopup(provider)
+		.then(function(result) {
+			var token = result.credential.accessToken;
+			// The signed-in user info.
+			var user = result.user;
+			console.log(user);
+			return ( 
+  			<div> Hi </div> ) 
+		})
+		.catch(function(error) {
+		// Handle Errors here.
+  		var errorCode = error.code;
+  		var errorMessage = error.message;
+		// The email of the user's account used.
+  		var email = error.email;
+  		// The firebase.auth.AuthCredential type that was used.
+  		var credential = error.credential;
+  		console.log(errorMessage)
+  		})
+  		return ( <div> Hi </div> ) 
 
-		return (
+  	}
+  		
 
-			<div>
-			<div className="card z-depth-0 card-summary">
-			<span className="card-title">
-			    <h5>Let me know everything about you!</h5></span>
-			</div>
-			<button className="waves-effect waves-light pink btn" onClick={this.props.signIn}>Sign In</button>
-			</div>
-			)
-	}
-}
 
-const mapStateToProps = (state) => {
-  return {
-  }
-}
+  	}
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  	signIn: () => dispatch(signIn())
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
+const mapStateToProps = (state) => {}
+
+
+
+export default connect(mapStateToProps)(SignIn)
