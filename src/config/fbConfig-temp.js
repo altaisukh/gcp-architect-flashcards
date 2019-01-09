d29f86e4c2621a952626ac6ad3b4a03aeaae59f8
Jess Lampe <jess.lampe@mavenwave.com>
2019-01-09 10:47:12 -0600

    Changed passwords

--- src/config/fbConfig-temp.js
+++ src/config/fbConfig-temp.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 
 // Replace this with your own config details, rename file as fbConfig.js
 var fbConfig = {
-    apiKey: "####################################",
-    authDomain: "####################################",
-    databaseURL: "####################################",
-    projectId: "####################################",
-    storageBucket: "####################################",
-    messagingSenderId: "####################################"
+    apiKey: "$FB_API",
+    authDomain: "$FB_AUTH_DOMAIN",
+    databaseURL: "$FB_DB_URL",
+    projectId: "$FB_PROJECT_ID",
+    storageBucket: "$FB_STORAGE_BUCKET",
+    messagingSenderId: "$FB_SENDER_ID"
   };
 firebase.initializeApp(fbConfig);
 firebase.firestore().settings({ timestampsInSnapshots: true });
